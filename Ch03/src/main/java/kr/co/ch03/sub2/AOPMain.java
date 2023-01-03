71cfbc0f7edacba7324b05237a29cea7ca7befc6
jojuyeong03 <jojuyeong03@gmail.com>
2023-01-03 16:52:56 +0900

    2023/01/03 수업

---
+++ Ch03/src/main/java/kr/co/ch03/sub2/AOPMain.java
@@ -0,0 +1,36 @@
+package kr.co.ch03.sub2;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import kr.co.ch03.sub2.AOPService;
+
+/*
+ * 날짜 : 2023/01/03
+ * 이름 : 조주영
+ * 내용 : 스프링 AOP 실습하기
+ * 
+ */
+public class AOPMain {
+	
+	public static void main(String[] args) {
+		
+		ApplicationContext ctx = new GenericXmlApplicationContext("application.xml");
+		
+		AOPService service = (AOPService) ctx.getBean("AOPService");
+		
+		System.out.println("--------------------");
+		service.insert();
+		
+		System.out.println("--------------------");
+		service.select();
+		
+		System.out.println("--------------------");
+		service.update();
+		
+		System.out.println("--------------------");
+		service.delete();
+		
+	}
+
+}
